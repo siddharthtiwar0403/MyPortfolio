26ea0f7e5f769e4bc1a9418e03c7dae0c9102770
Siddharth Tiwari <siddharthtiwari1350@gmail.com>
2025-12-25 17:58:12 +0530

    again same

--- netlify/functions/github.js
+++ netlify/functions/github.js
@@ -1,17 +1,42 @@
 export async function handler() {
-  const username = process.env.GITHUB_USERNAME;
   const token = process.env.GITHUB_TOKEN;
+  const username = process.env.GITHUB_USERNAME;
 
-  const res = await fetch(`https://api.github.com/users/${username}`, {
-    headers: {
-      Authorization: `Bearer ${token}`
+  const query = `
+    query($username: String!) {
+      user(login: $username) {
+        name
+        login
+        avatarUrl
+        bio
+        repositories { totalCount }
+        followers { totalCount }
+        following { totalCount }
+        starredRepositories { totalCount }
+        contributionsCollection {
+          contributionCalendar { totalContributions }
+          totalCommitContributions
+        }
+      }
     }
+  `;
+
+  const res = await fetch('https://api.github.com/graphql', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+      variables: { username }
+    })
   });
 
   const data = await res.json();
 
   return {
     statusCode: 200,
-    body: JSON.stringify(data)
+    body: JSON.stringify(data.data.user),
   };
 }
